e06c2e258de8ac288ee4ca6361fe8413ef664934
--replace-all <--add>
2023-06-05 13:49:52 +0530

    done

--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ app.use(cors())
 
 app.use(express.json())
 
-
 mongoose.connect("mongodb+srv://Aman_Mohadikar:V5FW1Y8X6b2pIiud@cluster0.gdww84s.mongodb.net/project-3")
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
